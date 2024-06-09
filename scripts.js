2c151f6c4aa0ac66ebccd90f72755fa79eaa852c
Rudy Stanga <rudystanga@Rudys-MacBook-Pro.local>
2024-06-08 23:58:01 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('payment-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Here you can add your payment processing logic
+    alert('Payment processing is not set up yet.');
+});
